993cba571c3b9638a317c17d161041709c70b2ea
김성은 <se.kim@okestro.com>
2024-08-26 18:26:32 +0900

    :sparkles: BaseballService 게임 진행 사용자 입력값 검증 로직 구현
    - 입력받은 숫자가 3자리가 아닐 경우 예외 발생
    - 입력받은 숫자가 숫자(1~9)가 아닐 경우 예외 발생

--- src/main/java/baseball/service/impl/BaseballServiceImpl.java
+++ src/main/java/baseball/service/impl/BaseballServiceImpl.java
@@ -2,6 +2,15 @@ package baseball.service.impl;
 
 import baseball.domain.BaseballNumber;
 import baseball.service.BaseballService;
+import baseball.util.BasicUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.regex.Pattern;
+
+import static baseball.exception.ErrorMessages.INVALID_NON_DIGIT;
+import static baseball.exception.ErrorMessages.INVALID_THREE_DIGIT;
+import static baseball.util.Constants.BASEBALL_NUMBER_SIZE;
+import static baseball.util.Constants.INPUT_NUMBER;
 
 
 public class BaseballServiceImpl implements BaseballService {
@@ -13,12 +22,49 @@ public class BaseballServiceImpl implements BaseballService {
     }
 
 
-
-
     @Override
     public void runGame() {
+        BasicUtils basicUtils = new BasicUtils();
+        while (true) {
+            try {
+                System.out.print(INPUT_NUMBER);
+                String inputNumbers = basicUtils.readLine();
+
+                this.isValidInputNumbers(inputNumbers);
+                // TODO: 사용자가 입력한 값도 중복체크
+
+                Integer[] convertToNumber = new Integer[BASEBALL_NUMBER_SIZE];
+                for (int i = 0; i < BASEBALL_NUMBER_SIZE; i++) {
+                    convertToNumber[i] = Character.getNumericValue(inputNumbers.charAt(i));
+                }
+
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
 
     }
+
+    private boolean isValidThreeDigit(String inputNumbers) {
+        return inputNumbers.length() > BASEBALL_NUMBER_SIZE;
+    }
+
+
+    private void isValidInputNumbers(String inputNumbers) {
+
+        if (isValidThreeDigit(inputNumbers)) {
+            throw new IllegalArgumentException(INVALID_THREE_DIGIT);
+        }
+
+        if(StringUtils.isBlank(inputNumbers)){
+            throw new IllegalArgumentException(INVALID_NON_DIGIT);
+        }
+        if (!Pattern.matches("^[1-9]+$", inputNumbers)) {
+            throw new IllegalArgumentException(INVALID_NON_DIGIT);
+        }
+    }
+
 
 
     @Override
