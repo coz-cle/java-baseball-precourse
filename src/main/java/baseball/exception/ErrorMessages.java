97b6a63c063eaf964ea321e201f45f2eb907f5b5
김성은 <se.kim@okestro.com>
2024-08-26 15:20:40 +0900

    :white_check_mark: ErrorMessage 예외 메세지 추가

--- src/main/java/baseball/exception/ErrorMessages.java
+++ src/main/java/baseball/exception/ErrorMessages.java
@@ -9,6 +9,11 @@ public class ErrorMessages {
      * 3. 게임 종료 후 1 또는 2가 입력되지 않을 경우 예외 발생
      */
 
-    public static final String NUMBER_RANGE_OVER = ""; // Ex
+    public static final String INVALID_THREE_DIGIT = "입력한 숫자가 3자리가 아닙니다. 3자리 숫자를 입력해주세요.";
+
+    public static final String INVALID_NON_DIGIT = "입력한 값이 숫자가 아닙니다. 1부터 9 사이의 숫자를 입력해주세요.";
+
+    public static final String INVALID_EXIT_CHOICE = "게임 종료 후 1 또는 2를 입력해야 합니다. 올바른 값을 입력해주세요.";
+
 
 }
