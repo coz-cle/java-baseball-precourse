515e513ed791b30ef9219d7e7cc92ed1059aacdf
김성은 <se.kim@okestro.com>
2024-08-27 18:11:35 +0900

    :sparkles: BasicUtils static 예약어 추가

--- src/main/java/baseball/util/BasicUtils.java
+++ src/main/java/baseball/util/BasicUtils.java
@@ -9,12 +9,12 @@ import static baseball.util.Constants.MAX_BASE_BALL_NUMBER;
 public class BasicUtils {
 
     // 1~9까지의 수 중에서 난수 제공
-    public int generateRandomNumber() {
+    public static int generateRandomNumber() {
         return Randoms.pickNumberInRange(Constants.MIN_BASE_BALL_NUMBER, MAX_BASE_BALL_NUMBER);
     }
 
     // 사용자 입력
-    public String readLine() {
+    public static String readLine() {
         return Console.readLine();
     }
 
