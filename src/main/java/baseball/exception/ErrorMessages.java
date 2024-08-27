6448327602032098b6a70497eb5682d324dfe14b
김성은 <se.kim@okestro.com>
2024-08-27 18:11:21 +0900

    :sparkles: 에러메세지 및 게임 관련 메세지 추가

--- src/main/java/baseball/exception/ErrorMessages.java
+++ src/main/java/baseball/exception/ErrorMessages.java
@@ -13,6 +13,8 @@ public class ErrorMessages {
 
     public static final String INVALID_NON_DIGIT = "입력한 값이 숫자가 아닙니다. 1부터 9 사이의 숫자를 입력해주세요.";
 
+    public static final String INVALID_UNIQUE_NUMBER = "중복된 숫자를 입력할 수 없습니다. 서로 다른 숫자를 입력해주세요.";
+
     public static final String INVALID_EXIT_CHOICE = "게임 종료 후 1 또는 2를 입력해야 합니다. 올바른 값을 입력해주세요.";
 
 
