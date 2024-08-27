cf8bbb4fee58415b1dda7e78058cbc1ec3d395ca
김성은 <se.kim@okestro.com>
2024-08-27 18:13:48 +0900

    :sparkles: BaseballNumber 멤버 변수 타입 변경

--- src/main/java/baseball/domain/BaseballNumber.java
+++ src/main/java/baseball/domain/BaseballNumber.java
@@ -1,23 +1,16 @@
 package baseball.domain;
 
-import baseball.util.BasicUtils;
-
 import java.util.HashSet;
 
+import static baseball.util.BasicUtils.generateRandomNumber;
 import static baseball.util.Constants.BASEBALL_NUMBER_SIZE;
 
 public class BaseballNumber {
 
-    private int first_number;
-    private int second_number;
-    private int third_number;
+    private final Integer[] baseball_numbers;
 
     public BaseballNumber() {
-        Integer[] numbers = settingUniqueNumbers();
-
-        this.first_number = numbers[0];
-        this.second_number = numbers[1];
-        this.third_number = numbers[2];
+        this.baseball_numbers = settingUniqueNumbers();;
     }
 
     /**
@@ -26,22 +19,16 @@ public class BaseballNumber {
      */
     private Integer[] settingUniqueNumbers() {
         HashSet<Integer> uniqueNumbers = new HashSet<>();
-         for (int i = 0; i < BASEBALL_NUMBER_SIZE; i++) {
-             uniqueNumbers.add(new BasicUtils().generateRandomNumber());
-         }
+
+        while (uniqueNumbers.size() < BASEBALL_NUMBER_SIZE) {
+            int number = generateRandomNumber();
+            uniqueNumbers.add(number);
+        }
+
          return uniqueNumbers.toArray(new Integer[uniqueNumbers.size()]);
      }
 
-
-    public int getFirst_number() {
-        return first_number;
-    }
-
-    public int getSecond_number() {
-        return second_number;
-    }
-
-    public int getThird_number() {
-        return third_number;
+    public Integer[] getBaseball_numbers() {
+        return baseball_numbers;
     }
 }
