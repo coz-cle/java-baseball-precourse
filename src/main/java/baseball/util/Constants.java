d860267d9e5e9f674fda89620783e6f9693d7844
김성은 <se.kim@okestro.com>
2024-08-28 16:31:15 +0900

    :sparkles: 게임 종료 관련 상수 추가

--- src/main/java/baseball/util/Constants.java
+++ src/main/java/baseball/util/Constants.java
@@ -21,11 +21,13 @@ public class Constants {
 
     // 게임 종료 관련 상수
     public static final String GAME_RESULT = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    public static final String GAME_RETRY = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String GAME_RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public static final String VALID_RESTART_REX_PATTERN = "^[1-2]+$";
     public static final String END_GAME = "게임 종료";
     public static final String RESTART_SIGNAL = "1";
     public static final String END_GAME_SIGNAL = "2";
+
+    public static final int END_GAME_CONDITION = 3;
 
 }
